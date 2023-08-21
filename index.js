8b039bfa8d6d8b4a0c9a55f3e3d159875240ff37
PowerMechGuy <powermechguy@gmail.com>
2023-08-21 19:58:54 -0400

    The Postman Problem

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -28,9 +29,27 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
+    // Moved to validateCourse() function
+    /*const schema = Joi.object({
+        name: Joi.string()
+        .min(3)
+        .required()
+    });*/
+    
+  if (error) {
     // 400 Bad request 
-    res.status(400).send('Name is required and should be at least 3 characters.')
+    res.status(400).send(error.details[0].message);
+    //res.status(400).send(result.error);
+    return;
+  }
+    
+    const result = schema.validate(req.body);
+    console.log(result);
+    
+  if (result.error) {
+    // 400 Bad request 
+    res.status(400).send(result.error.details[0].message);
+    //res.status(400).send(result.error);
     return;
   }
   const course = {
@@ -41,11 +60,44 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id',  (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // 404 means object not found
+  if (!course) res.status(404).send('The course with the given id was not found.');
+    
+  // Validate
+  // If Invalid, return 400 - Bad request 
+  //const result = validateCourse(req.body);
+  //Referencing error value in result object with
+  //object destructuring notation.
+  const { error } = validateCourse(req.body); // = result.error
+  
+  if (error) {
+    // 400 Bad request 
+    res.status(400).send(error.details[0].message);
+    //res.status(400).send(result.error);
+    return;
+  }
+    
+  // Update course
+  course.name = req.body.name;
+  // Return the updated course to the client
+  res.send(course);
+});
 
-
-
-
-
+function validateCourse(course) {
+  const schema = Joi.object({
+      name: Joi.string()
+      .min(3)
+      .required()
+  });
+    
+  const result = schema.validate(course);
+  
+  return schema.validate(course);
+}
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
@@ -54,6 +106,20 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res =>) {
+  // Look up the course
+  // Not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // 404 means object not found
+  if (!course) res.status(404).send('The course with the given id was not found.');
+  
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  
+  // Return the same course
+  res.send(course);
+});
 
 // PORT
 const port = process.env.PORT || 3000;
