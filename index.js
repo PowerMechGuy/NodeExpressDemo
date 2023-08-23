59038b87a400aaae8eb3062418618f759c3d27a4
PowerMechGuy <powermechguy@gmail.com>
2023-08-23 03:11:13 -0400

    Created configuration folder with basic configuration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -14,6 +15,12 @@ app.use(express.json()); // req.body
 app.use(express.urlencoded({ extended: true })); // key=value&key=value -> req.body
 app.use(express.static('public'));
 app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
 if (app.get('env') === 'development') {
     app.use(morgan('dev'));
     //app.use(morgan('combined'));
