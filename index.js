282d8e554d8fd6b0d144a6eebdf4417e0ef7be61
PowerMechGuy <powermechguy@gmail.com>
2023-08-23 03:23:54 -0400

    Setup debugging

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//const startupDebugger = require('debug')('app:startup');
+//const dbDebugger = require('debug')('app:db');
+const debug = require('debug')('app:general');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -7,8 +10,11 @@ const authenticator = require('./authenticator');
 const express = require('express');
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`);
+// Switching from console.log to debug
+//console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+//console.log(`app: ${app.get('env')}`);
+debug(`NODE_ENV: ${process.env.NODE_ENV}`);
+debug(`app: ${app.get('env')}`);
 
 app.use(express.json()); // req.body
 //app.use(express.urlencoded()); // key=value&key=value -> req.body
@@ -17,16 +23,27 @@ app.use(express.static('public'));
 app.use(helmet());
 
 // Configuration
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Server: ' + config.get('mail.host'));
-console.log('Mail Password: ' + config.get('mail.password'));
+//console.log('Application Name: ' + config.get('name'));
+//console.log('Mail Server: ' + config.get('mail.host'));
+//console.log('Mail Password: ' + config.get('mail.password'));
+// Switching from console.log to debug
+debug('Application Name: ' + config.get('name'));
+debug('Mail Server: ' + config.get('mail.host'));
+debug('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('dev'));
     //app.use(morgan('combined'));
     //app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    //console.log('Morgan enabled...');
+    // Replacing with call to startup debugger declared above
+    //startupDebugger('Morgan enabled...');
+    // Switching to general debug namespace
+    debug('Morgan enabled...');
 }
+
+// Db work...
+//dbDebugger('Connected to the database...');
 
 app.use(logger);
 
